1c7bb744411a6d04e7a8614aef949a716e3baa63
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-08-17 17:16:33 +0530

    added the add more notes functionality as the task, can be more optimized later

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,23 @@ function App() {
     }
   ]);
 
+  const [input , setInput] = useState("");
+
+  function addNote()
+  {
+    let newId = notes.length+1 ;
+    setNotes(prev=> {
+      return [...prev , {id : newId, data : input}]
+    })
+  }
+
+
   return (
     <div >
+     <div>
+       <input type="text" onChange={(e)=>setInput(e.target.value)} value={input}/>
+       <button onClick={() => addNote()}>Add</button>
+     </div>
       <Notes notes={notes} setnotes={setNotes}/>
 
     </div>
